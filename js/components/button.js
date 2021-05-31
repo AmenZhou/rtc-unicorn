d304dd7515995fea0d3d7ff3506e95572554ae81
amenzhou <chou.amen@gmail.com>
2021-05-31 19:10:16 -0400

    Add more audio c0 and c1 use ttpFile

--- js/components/button.js
+++ js/components/button.js
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { playCurrentAudio, stopPreviousAudio } from '../utils/audio_utils';
 import Tooltip from '@material-ui/core/Tooltip';
 import truncate from 'lodash/truncate';
 
 const Button = ({ phraseKey, src, text, id, ttp, ttpFile }) => {
+  const truncateTtp = truncate(ttp, { length: 500 });
+  const [ttpText, setTtpText] = useState(truncateTtp);
   const buttonHandler = e => {
     const audio = e.target.nextElementSibling;
 
     stopPreviousAudio();
-    playCurrentAudio(audio)
+    playCurrentAudio(audio);
   }
 
-  const truncateTtp = truncate(ttp, { length: 500 })
+  if (ttpFile) {
+    fetch(ttpFile)
+      .then(r => r.text())
+      .then(text => setTtpText(truncate(text, { length: 500 })))
+  }
 
   return <div className="box" key={`${phraseKey}/${id}`} >
-    <Tooltip title={<span style={{ fontSize: "min(1.7vmax, 12px)"}}>{truncateTtp}</span>}>
+    <Tooltip title={<span style={{ fontSize: "min(1.7vmax, 12px)"}}>{ttpText}</span>}>
       <button className="phrase" onClick={buttonHandler}>{text}</button>
     </Tooltip>
     <audio preload="auto" src={src}></audio>
