54443fc4244986add4f058120652acf84f7b70a2
amenzhou <chou.amen@gmail.com>
2021-05-31 22:06:54 -0400

    Adjust tooltip size

--- js/components/button.js
+++ js/components/button.js
@@ -2,9 +2,16 @@ import React, { useState } from 'react'
 import { playCurrentAudio, stopPreviousAudio } from '../utils/audio_utils';
 import Tooltip from '@material-ui/core/Tooltip';
 import truncate from 'lodash/truncate';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  tooltip: {
+    maxWidth: 500,
+  }
+}));
 
 const Button = ({ phraseKey, src, text, id, ttp, ttpFile }) => {
-  const truncateTtp = truncate(ttp, { length: 500 });
+  const truncateTtp = truncate(ttp, { length: 2000 });
   const [ttpText, setTtpText] = useState(truncateTtp);
   const buttonHandler = e => {
     const audio = e.target.nextElementSibling;
@@ -12,15 +19,20 @@ const Button = ({ phraseKey, src, text, id, ttp, ttpFile }) => {
     stopPreviousAudio();
     playCurrentAudio(audio);
   }
+  const classes = useStyles();
 
   if (ttpFile) {
     fetch(ttpFile)
       .then(r => r.text())
-      .then(text => setTtpText(truncate(text, { length: 500 })))
+      .then(text => setTtpText(truncate(text, { length: 2000 })))
   }
 
   return <div className="box" key={`${phraseKey}/${id}`} >
-    <Tooltip title={<span style={{ fontSize: "min(1.7vmax, 12px)"}}>{ttpText}</span>}>
+    <Tooltip
+      title={<span style={{ fontSize: "min(1.8vmax, 13px)"}}>{ttpText}</span>}
+      placement="right"
+      classes={{ tooltip: classes.tooltip }}
+    >
       <button className="phrase" onClick={buttonHandler}>{text}</button>
     </Tooltip>
     <audio preload="auto" src={src}></audio>
